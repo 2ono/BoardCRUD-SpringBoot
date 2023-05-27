a1b22b3cdef89e9da3c88eddf9fd3a6c3da98828
2ono <clt3555@gmail.com>
2023-05-27 15:23:38 +0900

    [add] board title and content display on html

--- src/main/java/com/crud/BoardStudyApplication.java
+++ src/main/java/com/crud/BoardStudyApplication.java
@@ -2,7 +2,9 @@ package com.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+//@ComponentScan(basePackages= {"BoardController"})
 @SpringBootApplication
 public class BoardStudyApplication {
 
--- src/main/java/com/crud/controller/BoardController.java
+++ src/main/java/com/crud/controller/BoardController.java
@@ -2,14 +2,24 @@ package com.crud.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
 
-	@GetMapping("/")
-	@ResponseBody
-	public String main() {
-		return "Hello World";
+	@GetMapping("/board/write")  // localhost:8080/board/write
+	public String boardWriteForm() {
+		
+		return "boardwrite";
+		
 	}
+	
+	@PostMapping("/board/writepro")
+	public String boardWritePro(String title, String content) {
+		System.out.println("제목 : " + title);
+		System.out.println("내용 : " + content);
+		return "";
+	}
+	
+	
 }
