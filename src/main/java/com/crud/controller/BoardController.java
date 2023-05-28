9ac673b7203ca9ab20fc91f701d5661d4f278cf6
2ono <clt3555@gmail.com>
2023-05-29 00:00:39 +0900

    [add] paging board

--- src/main/java/com/crud/controller/BoardController.java
+++ src/main/java/com/crud/controller/BoardController.java
@@ -3,6 +3,10 @@ package com.crud.controller;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +16,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.crud.entity.Board;
 import com.crud.service.BoardService;
+
+import net.bytebuddy.asm.Advice.OffsetMapping.Sort;
 
 @Controller
 public class BoardController {
@@ -39,8 +45,19 @@ public class BoardController {
 	}
 
 	@GetMapping("/board/list")
-	public String boardList(Model model) {
-		model.addAttribute("list", boardService.boardList());
+	public String boardList(Model model,
+			@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.DESC) Pageable pageable) {
+		
+		Page<Board> list = boardService.boardList(pageable);
+		
+		int nowPage = list.getPageable().getPageNumber() + 1;
+		int startPage = Math.max(nowPage - 4, 1);
+		int endPage = Math.min(nowPage + 5, list.getTotalPages());
+		
+		model.addAttribute("list", list);
+		model.addAttribute("nowPage", nowPage);
+		model.addAttribute("startPage", startPage);
+		model.addAttribute("endPage", endPage);
 		return "boardlist";
 	}
 
